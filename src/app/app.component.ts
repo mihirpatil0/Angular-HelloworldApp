7ce7890b0fec2ca919f2ae088a8f97d8583641d4
mihirpatil0 <mihirrpatil@gmail.com>
2021-09-07 01:28:43 +0530

    [mihir] Added: interpolation data binding.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Helloworl';
+  title = 'Helloworld';
+
+  ngOnInit(); void {
+    this.title = "Hello from Bridgelabz.";
+  }
 }
