d3d3206b6ec354318d06a94e3c80a21f1822e140
mihirpatil0 <mihirrpatil@gmail.com>
2021-09-08 21:11:12 +0530

    [mihir] Added: regexp to validate name.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   imgUrl="../assets/BridgeLabz.jpg";
   url = "https://www.bridgelabz.com";
   userName: string = "";
+  nameError: string = "";
 
 ngOnInit(): void {
     this.title = "Hello from Bridgelabz.";
@@ -18,5 +19,15 @@ ngOnInit(): void {
 onClick($event: any){
   console.log("save button is clicked!", $event);
   window.open(this.url, "_blank");
+  }
+
+onInput($event: any){
+  console.log("Change Event Occurred!", $event.data);
+  const nameRegex = RegExp('^[A-Z][a-zA-Z\\s]{2,}$');
+  if (nameRegex.test(this.userName)) {
+    this.nameError = "";
+    return;
+  }
+  this.nameError = "Name is Incorrect!";
   }
 }
